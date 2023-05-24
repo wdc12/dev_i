6c95be3fee03d1b02af878e9028c54f4c040b574
wdc12 <105369675+wdc12@users.noreply.github.com>
2023-05-24 23:25:43 +0530

    Update AI_SelectionSort.py

--- AI_SelectionSort.py
+++ AI_SelectionSort.py
@@ -19,3 +19,32 @@ for i in range(len(A)):
 print ("Sorted array")
 for i in range(len(A)):
     print("%d" %A[i],end=" , ")
+
+  
+  ''' n selection sort, the smallest value among the unsorted elements of the array is selected in every pass and inserted to its appropriate position into the array. It is also the simplest algorithm. It is an in-place comparison sorting algorithm. In this algorithm, the array is divided into two parts, first is sorted part, and another one is the unsorted part. Initially, the sorted part of the array is empty, and unsorted part is the given array. Sorted part is placed at the left, while the unsorted part is placed at the right.
+
+In selection sort, the first smallest element is selected from the unsorted array and placed at the first position. After that second smallest element is selected and placed in the second position. The process continues until the array is entirely sorted.
+
+The average and worst-case complexity of selection sort is O(n2), where n is the number of items. Due to this, it is not suitable for large data sets.
+
+Selection sort is generally used when -
+
+A small array is to be sorted
+Swapping cost doesn't matter
+It is compulsory to check all elements
+
+1. Time Complexity
+Case	Time Complexity
+Best Case
+O(n2)
+Average Case
+O(n2)
+Worst Case
+O(n2)
+
+2. Space Complexity
+Space Complexity
+O(1)
+Stable
+YES
+The space complexity of selection sort is O(1). It is because, in selection sort, an extra variable is required for swapping.'''
