7d663102ecf0a21be42eac9b6c17eec449d53608
wdc12 <105369675+wdc12@users.noreply.github.com>
2023-05-24 22:18:42 +0530

    Update AI_DFS.py

--- AI_DFS.py
+++ AI_DFS.py
@@ -19,3 +19,22 @@ def dfs_search(v1, graph, curr):
 
 dfs_search(v1, graph, '5')
 print()
+
+
+v2 = []
+q = []
+
+def bfs_search(v2, graph, curr):
+    v2.append(curr)
+    q.append(curr)
+
+    while len(q) > 0:
+        temp = q.pop(0)
+        print(temp, end = ' ')
+
+        for i in graph[temp]:
+            if i not in v2:
+                v2.append(i)
+                q.append(i)
+
+bfs_search(v2, graph, '5')
