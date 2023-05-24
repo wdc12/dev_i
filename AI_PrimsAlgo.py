1500db80b52dfb30466b02b3d4f5f153ddb0db6b
wdc12 <105369675+wdc12@users.noreply.github.com>
2023-05-24 23:29:50 +0530

    Update AI_PrimsAlgo.py

--- AI_PrimsAlgo.py
+++ AI_PrimsAlgo.py
@@ -51,3 +51,29 @@ if __name__ == '__main__':
                [0, 5, 7, 9, 0]]
  
     g.primMST()
+
+  
+  '''
+  Spanning tree - A spanning tree is the subgraph of an undirected connected graph.
+
+Minimum Spanning tree - Minimum spanning tree can be defined as the spanning tree in which the sum of the weights of the edge is minimum. The weight of the spanning tree is the sum of the weights given to the edges of the spanning tree.
+
+Now, let's start the main topic.
+
+Prim's Algorithm is a greedy algorithm that is used to find the minimum spanning tree from a graph. Prim's algorithm finds the subset of edges that includes every vertex of the graph such that the sum of the weights of the edges can be minimized.
+
+Prim's algorithm starts with the single node and explores all the adjacent nodes with all the connecting edges at every step. The edges with the minimal weights causing no cycles in the graph got selected.
+
+How does the prim's algorithm work?
+Prim's algorithm is a greedy algorithm that starts from one vertex and continue to add the edges with the smallest weight until the goal is reached. The steps to implement the prim's algorithm are given as follows -
+
+First, we have to initialize an MST with the randomly chosen vertex.
+Now, we have to find all the edges that connect the tree in the above step with the new vertices. From the edges found, select the minimum edge and add it to the tree.
+Repeat step 2 until the minimum spanning tree is formed.
+The applications of prim's algorithm are -
+
+Prim's algorithm can be used in network designing.
+It can be used to make network cycles.
+It can also be used to lay down electrical wiring cables.
+
+o(|V^2|) '''
