133bc187f68e4e69c4916b12e8e5ef664039a7d4
wdc12 <105369675+wdc12@users.noreply.github.com>
2023-05-24 22:24:12 +0530

    Update AI_BDFS.py
    Done

--- AI_BDFS.py
+++ AI_BDFS.py
@@ -16,7 +16,9 @@ def dfs_search(v1, graph, curr):
 
         for i in graph[curr]:
             dfs_search(v1, graph, i)
-
+            
+            
+print("DFS:", end=" ")
 dfs_search(v1, graph, '5')
 print()
 
@@ -37,4 +39,36 @@ def bfs_search(v2, graph, curr):
                 v2.append(i)
                 q.append(i)
 
+print("BFS:", end=" ")
 bfs_search(v2, graph, '5')
+
+'''Depth First Search:
+Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures.
+The algorithm starts at the root node (selecting some arbitrary node as the root node in the case of
+a graph) and explores as far as possible along each branch before backtracking.
+Performance Measure:
+d = the depth of the search tree = the number of levels of the search tree.
+n = number of nodes in level .
+i
+Time complexity: Equivalent to the number of nodes traversed in DFS.
+T(n) = 1 + n 2 + n3+ n4+ …..+nd = O(nd)
+Space complexity: Equivalent to how large can the fringe get.
+S(n) = O(n*d)
+Completeness: DFS is complete if the search tree is finite, meaning for a given finite search tree,
+DFS will come up with a solution if it exists.
+Optimality: DFS is not optimal, meaning the number of steps in reaching the solution, or the cost
+spent in reaching it is high.
+Breadth First Search:
+Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data
+structures. It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a
+‘search key’), and explores all of the neighbour nodes at the present depth prior to moving on to
+the nodes at the next depth level.
+d = the depth of the shallowest solution.
+ni= number of nodes in level .
+Time complexity: Equivalent to the number of nodes traversed in BFS until the shallowest
+solution.
+T(n) = 1 + n 2 + n3+ n4+ …..+nd = O(nd)
+Space complexity: Equivalent to how large can the fringe get.
+S(n) = O(nd)
+Completeness: BFS is complete, meaning for a given search tree, BFS will come up with a
+solution if it exists.'''
